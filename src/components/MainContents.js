8ece63fb0d2118a38ca55662c5d0ff3c244d2a49
dlatldhs <sionzz1713@gmail.com>
2022-06-13 21:14:16 +0900

    test

--- src/components/MainContents.js
+++ src/components/MainContents.js
@@ -2,10 +2,17 @@ export default function MainContents() {
   return (
     <div className="main">
       <div className="index">
-        <img src="https://media.gucci.com/content/HeroBigStandard_1600x760/1653404405/HeroBigStandard_Oura-ring-hp_001_Default.jpg" />
+        <img src="https://media.gucci.com/content/HeroBigStandard_1600x760/1653404405/HeroBigStandard_Oura-ring-hp_001_Default.jpg" alt="main" />
       </div>
       <div className="Collabo-item">
-        <img src="./imgs/main_contents_image_1.webp" />
+        <img src="./imgs/main_contents_image_1.webp" alt="main_contents" />
+        <h2>[구찌 홀스빗 1955] 아디다스 x 구찌 미니백</h2>
+        <div className="ditail-button-a">
+          <a href="./index/">자세히 보기</a>
+        </div>
+      </div>
+      <div className="long-picture">
+        <img src="./imgs/main_contents_long_image.jpg" alt="long_img" />
       </div>
     </div>
   );
