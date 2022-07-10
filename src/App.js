868459ff98f0808973a8705ff6bfbcbe8f0a54b2
dlatldhs <sionzz1713@gmail.com>
2022-07-10 22:29:37 +0900

    react router 사용

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer";
 import MainContents from "./components/MainContents";
 import SliderData from "./components/SliderData";
 import ImageSlider from "./components/ImageSlider";
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import NotFound from "./components/NotFound";
 
 import "./headerStyle.css";
 import "./mainStyle.css";
@@ -10,6 +12,7 @@ import "./footerStyle.css";
 import "./slider.css";
 import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
+import GucciItems from "./components/GucciItems";
 
 export default function App() {
   const [position, setPosition] = useState(0);
@@ -26,9 +29,18 @@ export default function App() {
 
   return (
     <div className="App">
-      <Header pwd={position} />
-      <MainContents />
-      <Footer />
+      <BrowserRouter>
+        <Header pwd={position} />
+        <Routes>
+          {/* <MainContents />
+          <GucciItems />
+          <Footer /> */}
+          <Route path="/a" element={<MainContents />}></Route>
+          <Route path="/a" element={<Footer />}></Route>
+          <Route path="/b" element={<GucciItems />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
       {/* <ImageSlider slides={SliderData}/> */}
     </div>
   );
---
+++ src/components/GucciItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function GucciItems( props ){
+    return (
+        <div>
+            <img src="//media.gucci.com/style/DarkGray_South_0_160_316x316/1501263906/500877_DRW00_9522_001_100_0000_Light-.jpg" alt="[롸이톤] 남성 구찌 로고 롸이톤 레더 스니커즈" />
+            <h2>[롸이톤] 남성 구찌 로고 롸이톤 레더 스니커즈</h2>
+            <h3>1,340,000 $</h3>
+        </div>
+    )
+}
